dea26afcc150a26e10d984bf5e2e197e8c55c7b6
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2021-04-18 22:18:29 +0200

    add INNER JOIN car

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 
 exports.homeGet = async (req, res) => {
-      const marque = await querysql('SELECT name from marque')
-
-      res.render('home', {marque});
+      const car = await querysql('SELECT * FROM car INNER JOIN serie ON car.serie_id = serie.serie_id INNER JOIN modele ON serie.modele_id = modele.modele_id INNER JOIN marque ON modele.marque_id = marque.marque_id');
+      console.log(car);
+      res.render('home', {car});
    };
